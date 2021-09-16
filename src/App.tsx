41ae01f5b154c3830c8ccd8b27adfc4ffc6efd20
Mahandrisoa <andriamaherisoamahandrisoa@gmail.com>
2021-09-16 16:22:05 +0200

    Hello voplosy

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1>Hello from V2</h1>
+          <h1>Hello from V2 Liantsoa Voplôsy</h1>
         </header>
       </div>
   );
